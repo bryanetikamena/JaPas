74443c83569481d554a79caf4d7a7b6521516b22
Nama Anda <bryanetikamena@gmail.com>
2023-12-18 15:23:43 +0700

    Fix error

---
+++ data.js
@@ -0,0 +1,71 @@
+export const CategoryList = [
+    {
+      id: 1,
+      categoryName: 'klepon',
+    },
+    {
+      id: 2,
+      categoryName: 'putu_mayang',
+    },
+    {
+      id: 3,
+      categoryName: 'kue_lumpur',
+    },
+    {
+      id: 4,
+      categoryName: 'kue_putu',
+    },
+    {
+      id: 5,
+      categoryName: 'clorot',
+    },
+    {
+      id: 6,
+      categoryName: 'getuk',
+    },
+  ];
+  export const BlogList = [
+    {
+      id: 1,
+      title: 'Klepon',
+      category: 'klepon',
+      image:
+        'https://cdn0-production-images-kly.akamaized.net/_b9WmjBzuFHyUQx5XEN-qPyMLPw=/0x33:999x596/680x383/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/3517578/original/016392300_1626936713-shutterstock_1887053635.jpg',
+    },
+    {
+      id: 2,
+      title: 'Putu Mayang',
+      category: 'putu_mayang',
+      image:
+        'https://static.promediateknologi.id/crop/213x309:762x674/750x500/webp/photo/2023/01/10/3898363264.png',
+    },
+    {
+      id: 3,
+      title: 'Kue Lumpur',
+      category: 'kue_lumpur',
+      image:
+        'https://cdn0-production-images-kly.akamaized.net/1xgLUhBpJDG0NYoHK9qcHowEjhg=/0x1869:3399x3785/680x383/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/3553153/original/046489900_1630055222-shutterstock_1752501980.jpg',
+    },
+    {
+      id: 4,
+      title: 'Kue Putu',
+      category: 'kue_putu',
+      image:
+        'https://cdn.rri.co.id/berita/90/images/1676791039080-IMG_20230219_141115/1676791039080-IMG_20230219_141115.jpg',
+    },
+    {
+      id: 5,
+      title: 'Clorot',
+      category: 'clorot',
+      image:
+        'https://cdn0-production-images-kly.akamaized.net/ApDzprdPcQ9kGMNNpFOy_Oi-e-s=/1x85:1000x648/680x383/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/3910231/original/096750300_1642729627-shutterstock_2043564920.jpg',
+    },
+    {
+      id: 6,
+      title: 'Getuk',
+      category: 'getuk',
+      image:
+        'https://cdn.idntimes.com/content-images/community/2020/11/fromandroid-a3a5b11f20bee41a2706a405f86c0c38_600x400.jpg',
+    },
+    
+  ];
